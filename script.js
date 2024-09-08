088e8050bc806adc4d9170a3f6bab41bd433a453
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-08 14:22:24 +0530

    2) Values and Variables
    learn about declaration of variable 
    Assigning of values to variables

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-let js = "hello";                                
-if (js === "hello") alert("js is FUn!!");        //if the conditions is true it show an alert on the browser page!
+console.log(20+10+30+40);           //print the result as 100 in console
 
-console.log(40 + 20 + 30 - 40);    //printing the value in console
+console.log("john");             //print john in console
+console.log(20);                    //print 20 in console
+
+// decaliring variables
+let firstname = "vinayak";          //first name is a varibale which can store a data
+console.log(firstname);
+
+
+// let 3num = 3;n                   //show error 
+let num3 = 3;                    //right way
+// let john&seena = "fighter";     //show error 
+let john$seena = "fighter";         //only use $ in varibel name.expect this spcl key it show error
+// let new = "new"                  //show error "new is keyword"
+
+// cannot start with number in variable  eg:- 3year = 3; --->it show syntax error
+// cannot use specialkeys expect $ and keywords
+
+
+// write camalcase ===> firstNamePerson = "vinu"
+//                 ===> first_name_person = "vinu;"
+let first_name = "vinayak";          //ruby
+let lastName = "dev";               //cammel case
+
+
+//write constant values name in capital otherwise dont use it ,
+// let pi = 3.1415;              //will not show but its wrong method of writing code
+let PI = 3.1415;                 //write in full capital like this "PI"
+
+// can use only numbers,underscore,letters in variable
+
