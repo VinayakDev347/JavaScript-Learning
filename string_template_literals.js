e8378438f532e37f0248a3991b3ce766d4d16fac
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-09 00:26:57 +0530

    Update string_template_literals.js
    Types of Conversion and Coercion

--- string_template_literals.js
+++ string_template_literals.js
@@ -35,3 +35,20 @@ if (birthYear <= 2000){
     centuary =21;
 }
 console.log(centuary)
+
+//------------Type conversion--------------------
+const inputYear = '2021';           //string
+console.log(Number(inputYear));
+console.log(Number(inputYear)+18);          //202118, we need to get 2039 so call Number(str) it change string to number!
+
+console.log(Number("vinu"));            //NaN = NOt an number ,its type is numnber but the value of the number is invalid!
+console.log(typeof(NaN))
+
+console.log(String(23),23);
+
+// ----------Type coercion------------------------
+console.log(`I am `+ 23 + `years old`);     // I am 23years old
+console.log(`23` - `10` - 3);               //10
+console.log(`23` / `2`);                    //11.5
+console.log(`10`-`4`-`3`-2+`5`)             //15
+console.log(2+3+4+`5`);                     //95
