608fb83cf84613a442208d92c75029dd53fdfb20
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-08 13:15:01 +0530

    explained code of js
    learn to print in console and connect js file to html

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let js = "hello";
-if (js === "hello") alert("js is FUn!!");
+let js = "hello";                                
+if (js === "hello") alert("js is FUn!!");        //if the conditions is true it show an alert on the browser page!
 
-console.log(40 + 20 + 30 - 40);
+console.log(40 + 20 + 30 - 40);    //printing the value in console
