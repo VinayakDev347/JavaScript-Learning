50dfadff7245c36044a622671c08953f6189c0e2
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-08 15:13:21 +0530

    3. Data types
    learn 2 data types :primitive and non primitive
    
    primitive(number.string,boolean,undefined,null,symbol,BigInt)
    non-primitive(list,objects)

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
-let js = "hello";                                
-if (js === "hello") alert("js is FUn!!");        //if the conditions is true it show an alert on the browser page!
+/*
+ Data types 
 
-console.log(40 + 20 + 30 - 40);    //printing the value in console
+1.primitive (hold single values)
+
+String
+Number
+Bigint
+Boolean
+Undefined
+Null
+Symbol
+
+2. Non-primitive Data type
+
+Object
+List 
+ */
+
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);           //true
+
+console.log(typeof true);               //boolean
+console.log(typeof javaScriptIsFun);    //boolean
+console.log(typeof 23);                 //number
+console.log(typeof "vinu");             //string
+
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun); 
+
+let year;
+console.log(typeof year);               //undefined
+
+year=2001;
+console.log(typeof year);               //number
+
+console.log(typeof null);               //object
