d98dbdd5137f46debcd64db2ca519ca0281771b6
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-08 17:37:16 +0530

    Create Q_find_BMI.js
    Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / (height * height) (mass in kg and height in meters).
    
    Your task is to write some code to help them:
    
    Store Mark's and John's mass and height in variables called massMark, heightMark, massJohn and heightJohn.
    
    Calculate both their BMIs using the formula, and store the results in two variables called BMIMark and BMIJohn.
    
    Log the value of BMIMark and BMIJohn to the console.
    
    BONUS: Create a boolean variable markHigherBMI containing information about whether Mark has a higher BMI than John. Log it to the console too
    
    TEST DATA 1: Marks weighs 78 kg and is 1.69 m tall. John weighs 92 kg and is 1.95 m tall.

---
+++ Q_find_BMI.js
@@ -0,0 +1,11 @@
+//Q to find BMI of 2 person mark and john 
+let massMark = 78;
+let heightMark = 1.69;
+let massJohn = 92;
+let heightJohn = 1.95;
+
+let BMIMark  = massMark / (heightMark * heightMark);
+console.log("BMI of Mark is :",BMIMark )
+
+let BMIJohn  = massJohn / (heightJohn * heightJohn);
+console.log("BMI of Mark is :",BMIJohn )
