69bd1736f50c867cd49ff9e8a4f9060110e0f319
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-10 00:54:55 +0530

    1st_part_learn.js
    all inserted in

--- learn.js
+++ learn.js
@@ -1,4 +1,44 @@
 /*
+let js = "hello";
+if (js === "hello") alert("js is FUn!!");
+
+console.log(40 + 20 + 30 - 40);
+//-------------------------------------------
+
+console.log(20+10+30+40);           //print the result as 100 in console
+
+console.log("john");             //print john in console
+console.log(20);                    //print 20 in console
+
+// decaliring variables
+let firstname = "vinayak";          //first name is a varibale which can store a data
+console.log(firstname);
+
+
+// let 3num = 3;n                   //show error 
+let num3 = 3;                    //right way
+// let john&seena = "fighter";     //show error 
+let john$seena = "fighter";         //only use $ in varibel name.expect this spcl key it show error
+// let new = "new"                  //show error "new is keyword"
+
+// cannot start with number in variable  eg:- 3year = 3; --->it show syntax error
+// cannot use specialkeys expect $ and keywords
+
+
+// write camalcase ===> firstNamePerson = "vinu"
+//                 ===> first_name_person = "vinu;"
+let first_name = "vinayak";          //ruby
+let lastName = "dev";               //cammel case
+
+
+//write constant values name in capital otherwise dont use it ,
+// let pi = 3.1415;              //will not show but its wrong method of writing code
+let PI = 3.1415;                 //write in full capital like this "PI"
+
+// can use only numbers,underscore,letters in variable
+
+
+// -------------------------------------------
  Data types 
 
 1.primitive (hold single values)
@@ -257,4 +297,33 @@ const day = 'friday';
         console.log("Enter any valid day broo!!!")
         break;
  }
+
+
+//----------condition (ternary) operator-------------
+
+//THIS HAS 3PARTS SO ITS CALLED TERNARY OPERATOR
+
+// 1.first method
+const age = 1;
+age >= 18 ? console.log("can drink wine!"):
+console.log("can drink only water!!")           //can drink only water!!
+
+// 2.second method
+const age1 = 15
+const drink = age1 >= 18 ? 'wine': 'water '; 
+console.log(drink);                             //water
+
+// 3.third method
+const age3 = 21;
+let drink2;
+if(age3>=18){
+    drink2="wine";
+}else{
+    drink2="water";
+}
+console.log(drink2);
+
+// 4.forth method
+console.log(`i linke to drink ${age3 >= 18 ? 'wine': 'water'}`);
 */
+
