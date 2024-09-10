b7fedab8e9eb7c7a04da697ab7b9baf57d237e49
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-10 21:19:58 +0530

    part2.js
    part2 of learning js
    
    usage of strict mode in js

---
+++ part2.js
@@ -0,0 +1,17 @@
+'use strict';  //helps to find error in code easily
+
+///////////////////////////////////////
+// Activating Strict Mode
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) hasDriversLicense = true;                
+if (hasDriversLicense) console.log('I can drive :D');        //i can drive:D -----because of using strictmode if error occur it shows in console  as warning!!!
+
+// const interface = 'Audio';
+// const private = 534;             //cant use private as variable name
+
+
+///////////////////////////////////////
+
+ 
