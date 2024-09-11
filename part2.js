dd32fc5fd62cb962234b66d993e3e68c41ec7ec1
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-11 22:26:51 +0530

    part2_2.js
    declairing fucntions
    arrow function

--- part2.js
+++ part2.js
@@ -44,3 +44,41 @@ console.log(appleJuice);                //Juice with 5 apples and 0 oranges!!
 
 const orangeJuice = fruitProcess(3,7);
 console.log(orangeJuice);               //Juice with 3 apples and 7 ooranges!!
+
+
+//---------------------------------------------------------------------------------------
+//1 function declaration (we can call where ever we want)
+function calAge1(birthYear){
+  return 2024-birthYear;
+}
+
+const age1 = calAge1(2001);
+// console.log(age1)         //23
+
+//2 : Function expression(Anonymous function )
+const calAge2 = function(birthYear){
+  return 2024-birthYear;
+}
+
+const age2 = calAge2(1991);
+console.log(age1,age2)        //23 33
+*/
+
+//---------------------arrow function -----start
+const calAge2 = function(birthYear){
+  return 2024-birthYear;
+}
+//arrow function
+const calAge3 = birthYear => 2024-birthYear;
+const age3 = calAge3(1991);                     
+console.log(age3);                              //33
+
+
+const yearUntilRetirement = (birthYear,name) => {
+  const age = 2024-(birthYear);
+  const retirement = 65 - age;
+  return `${name} retires in ${retirement} years!!`
+}
+
+console.log(yearUntilRetirement(1991,'johny'));     //johny retires in 32 years!!
+console.log(yearUntilRetirement(2001,'viknu'));     //viknu retires in 42 years!!
