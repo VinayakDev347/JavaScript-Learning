e471b4e45ba2cb377e6970ea595584fa80de8a17
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-12 21:30:29 +0530

    Update challenge.js
    tried one more test on the funciton

--- challenge.js
+++ challenge.js
@@ -22,12 +22,12 @@ GOOD LUCK 😀
 
 
 const calcAverage = (a, b, c) => (a + b + c) / 3;
-console.log(calcAverage(3, 4, 5));                  //finding average
+console.log(calcAverage(3, 4, 5));                  //finding average = 4
 
 // Test 1
 let scoreDolphins = calcAverage(44, 23, 71);
 let scoreKoalas = calcAverage(65, 54, 49);
-console.log(scoreDolphins, scoreKoalas);            //
+console.log(scoreDolphins, scoreKoalas);            
 
 const checkWinner = function (avgDolphins, avgKoalas) {
   if (avgDolphins >= 2 * avgKoalas) {
@@ -38,6 +38,12 @@ const checkWinner = function (avgDolphins, avgKoalas) {
     console.log('No team wins...');
   }
 }
-checkWinner(scoreDolphins, scoreKoalas);//
 
-checkWinner(576, 111);
+// checkWinner(scoreDolphins, scoreKoalas);    
+checkWinner(576, 111);                        //Dolphins win 🏆 (576 vs. 111)
+
+// Test 2
+scoreDolphins = calcAverage(85, 54, 41);      //60
+scoreKoalas = calcAverage(23, 34, 27);        //28
+console.log(scoreDolphins, scoreKoalas);      //60 28
+checkWinner(scoreDolphins, scoreKoalas);      //Dolphins win 🏆 (60 vs. 28)
