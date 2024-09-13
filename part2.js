1368da4ec4dad3b9b09f36442c0a92d36b710f08
Vinayak Dev <vinayakdev347@gmail.com>
2024-09-13 23:11:07 +0530

    Update part2.js
    object

--- part2.js
+++ part2.js
@@ -203,3 +203,21 @@ if (friends.includes('Steven')) {
 
 
 //////////---------------------------------------------------------------------
+//------------ Introduction to Objects-----------------
+
+const jonasArray = [
+  'Jonas',
+  'Schmedtmann',
+  2037 - 1991,
+  'teacher',
+  ['Michael', 'Peter', 'Steven']
+];
+
+const jonas = {                                 //object(in key value pair) name jonas and properties are given below
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+//-------------------------------------------------------------
